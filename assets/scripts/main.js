479db2564076b2d171f052a1909e250c10fd70a9
pablo <pjcanegallo9@hotmail.com>
2023-03-06 19:40:18 -0300

    borrando archivo amazing y pegado de nuevo por cambios erroneos realizados

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,19 @@
 import data from "./amazing.js";
 
-console.log(data);
+const card = document.getElementById("card");
+let tarjetas = '';
 
-
-
+for(d of data) {
+    tarjetas += `
+        <div class="card pt-2 shadow" style="width: 15rem;">
+            <img src="${d.events.image}" class="card-img-top w-100 h-50 b_shadow" alt="party">
+            <div class="card-body text-center">
+                <h5 class="card-title"></h5>
+                <p class="card-text"></p>
+                <span class="card-text pe-2 ps-3">Price: </span>
+                <a href="./details.html" class="btn btn-danger b_shadow_button">More</a>
+            </div>
+        </div>
+    `
+}
+console.log(tarjetas)
