071d23b88cf948dcca3a33d9faf0a774012d6b99
pablo <pjcanegallo9@hotmail.com>
2023-03-07 08:28:44 -0300

    termino for para agregar cards

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,23 @@
 import data from "./amazing.js";
 
-const card = document.getElementById("card");
+
+const card = document.getElementById("card")
+
+
 let tarjetas = '';
 
-for(d of data) {
+for (let d of data.events) {
     tarjetas += `
         <div class="card pt-2 shadow" style="width: 15rem;">
-            <img src="${d.events.image}" class="card-img-top w-100 h-50 b_shadow" alt="party">
+            <img src="${d.image}" class="card-img-top w-100 h-50 b_shadow" alt="party">
             <div class="card-body text-center">
-                <h5 class="card-title"></h5>
-                <p class="card-text"></p>
-                <span class="card-text pe-2 ps-3">Price: </span>
+                <h5 class="card-title">${d.name}</h5>
+                <p class="card-text">${d.description}</p>
+                <span class="card-text pe-2 ps-3">Price: ${d.price}</span>
                 <a href="./details.html" class="btn btn-danger b_shadow_button">More</a>
             </div>
         </div>
     `
 }
-console.log(tarjetas)
+
+card.innerHTML = tarjetas
