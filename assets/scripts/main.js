cf251f13f6f379960fe02fc3a7e246cee6430919
pablo <pjcanegallo9@hotmail.com>
2023-03-07 10:39:03 -0300

    corrigiendo estilos de cards

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,17 @@
-import data from "./amazing.js";
-
-
+import data from "./amazing.js"
 const card = document.getElementById("card")
-
-
-let tarjetas = '';
+let tarjetas = ''
 
 for (let d of data.events) {
     tarjetas += `
         <div class="card pt-2 shadow" style="width: 15rem;">
-            <img src="${d.image}" class="card-img-top w-100 h-50 b_shadow" alt="party">
+            <img src="${d.image}" class="card-img-top w-100 b_shadow altura" alt="party">
             <div class="card-body text-center">
                 <h5 class="card-title">${d.name}</h5>
                 <p class="card-text">${d.description}</p>
-                <span class="card-text pe-2 ps-3">Price: ${d.price}</span>
+            </div>
+            <div class="card-footer d-flex justify-content-around align-items-center">
+                <span class="card-text ">Price: ${d.price}</span>
                 <a href="./details.html" class="btn btn-danger b_shadow_button">More</a>
             </div>
         </div>
