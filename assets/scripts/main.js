776a8b457b6125ebe98fd5e531abebda57cee659
pablo <pjcanegallo9@hotmail.com>
2023-03-08 03:25:02 -0300

    corregido details y demas

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ let tarjetas = ''
 for (let d of data.events) {
     tarjetas += `
         <div class="card pt-2 mb-4 shadow" style="width: 15rem;">
-            <img src="${d.image}" class="card-img-top w-100 b_shadow altura" alt="party">
+            <img src="${d.image}" class="card-img-top w-100 b_shadow altura ofc" alt="party">
             <div class="card-body text-center">
                 <h5 class="card-title">${d.name}</h5>
                 <p class="card-text">${d.description}</p>
--- assets/scripts/past.js
+++ assets/scripts/past.js
@@ -6,7 +6,7 @@ for (let d of data.events) {
     if (d.date < data.currentDate) {
         tarjetas += `
             <div class="card pt-2 mb-4 shadow" style="width: 15rem;">
-                <img src="${d.image}" class="card-img-top w-100 b_shadow altura" alt="party">
+                <img src="${d.image}" class="card-img-top w-100 b_shadow altura ofc" alt="party">
                 <div class="card-body text-center">
                     <h5 class="card-title">${d.name}</h5>
                     <p class="card-text">${d.description}</p>
